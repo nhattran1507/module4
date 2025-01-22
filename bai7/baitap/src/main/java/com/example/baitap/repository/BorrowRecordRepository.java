f83921ced01ae5f6a580601db714f1831b62a8c1
C0924G1-TRANMINHNHAT <minhnhat1507@gmail.com>
2025-01-22 08:55:00 +0700

    baitap

---
+++ bai7/baitap/src/main/java/com/example/baitap/repository/BorrowRecordRepository.java
@@ -0,0 +1,9 @@
+package com.example.baitap.repository;
+
+
+import com.example.book_management.model.BorrowRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Integer> {
+    BorrowRecord findByBorrowCodeAndReturnedFalse(String borrowCode);
+}
---
+++ bai7/baitap/src/main/java/com/example/baitap/service/IBorrowRecordService.java
@@ -0,0 +1,16 @@
+package com.example.baitap.service;
+
+import com.example.book_management.model.BorrowRecord;
+
+import java.util.List;
+
+public interface IBorrowRecordService {
+    List<BorrowRecord> getAll();
+    void save(BorrowRecord blog);
+    void update(int id, BorrowRecord blog);
+    void remove(int id);
+    BorrowRecord findById(int id);
+    BorrowRecord findByBorrowCodeAndReturnedFalse(String borrowCode);
+    String borrowBook(Integer bookId);
+    void returnBook(String borrowCode);
+}
